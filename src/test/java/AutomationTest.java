4039d62927c784429227b8b7e0ccc20aaae0b57c
d1508 <101516279+d1508@users.noreply.github.com>
2022-04-03 21:25:16 +0300

    Add files via upload

--- src/test/java/AutomationTest.java
+++ src/test/java/AutomationTest.java
@@ -1,8 +1,6 @@
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 public class AutomationTest {
 
@@ -11,7 +9,7 @@ public class AutomationTest {
     @Test
     public void getUrl() {
 
-        AutomationPractice gUrl = new AutomationPractice("C:\\Users\\david\\Desktop\\SELENIUM\\chromedriver.exe");
+        SignInAutomation gUrl = new SignInAutomation("C:\\Users\\david\\Desktop\\SELENIUM\\chromedriver.exe");
         Assert.assertEquals(gUrl.SignUpTest(), "http://automationpractice.com/index.php");
     }
 
@@ -19,8 +17,8 @@ public class AutomationTest {
     // Test of Sign In
     @Test
     public void signIn() throws InterruptedException {
-        AutomationPractice sIn = new AutomationPractice("C:\\Users\\david\\Desktop\\SELENIUM\\chromedriver.exe");
-        Assert.assertEquals(sIn.signInAutomation(), "My account - My Store");
+        SignInAutomation sIn = new SignInAutomation("C:\\Users\\david\\Desktop\\SELENIUM\\chromedriver.exe");
+        Assert.assertEquals(sIn.signInAuto(), "My account - My Store");
 
     }
 
